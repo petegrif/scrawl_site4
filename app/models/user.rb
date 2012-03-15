801da44013ac9c6c235153a12ef3db9528aef16f
Peter Griffiths <petegrif@gmail.com>
2012-03-15 00:26:46 -0700

    display additional user info

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,8 @@
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :name, :email, :password, :password_confirmation
+  attr_accessible :name, :email, :password, :password_confirmation, :handle, 
+                  :tagline, :location, :fb_url, :likes, :dislikes
   has_secure_password
 
   has_many :scrawls, dependent: :destroy
